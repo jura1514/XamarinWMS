3825dd818a0759602c516d8dfdb4f9da5bc0ca5f
George <georgij.ng@gmail.com>
2016-10-30 18:29:58 +0000

    changed models to model

--- XamarinWMS/XamarinWMS/Model/DeliveryData.cs
+++ XamarinWMS/XamarinWMS/Model/DeliveryData.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace XamarinWMS.Models
+namespace XamarinWMS.Model
 {
     public class DeliveryData
     {
--- XamarinWMS/XamarinWMS/View/Tagging.xaml.cs
+++ XamarinWMS/XamarinWMS/View/Tagging.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
-using XamarinWMS.Models;
+using XamarinWMS.Model;
 using ZXing.Net.Mobile.Forms;
 
 namespace XamarinWMS.View

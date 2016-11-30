86a02a2b6fab2bb5575e3073c1822c8300d0b513
George <George@DESKTOP-TEIVNGB>
2016-11-30 23:20:53 +0000

    small constrants changes to web service

--- XamarinWMS/XamarinWMS/Constants.cs
+++ XamarinWMS/XamarinWMS/Constants.cs
@@ -9,9 +9,9 @@ namespace XamarinWMS
     public static class Constants
     {
         // URL of REST service
-        public static string RestUrl = "http://localhost:52326/api/deliveries{0}";
+        public static string RestUrl = "http://192.168.0.37:52326/api/Delivery";
         // Credentials that are hard coded into the REST service
-        public static string Username = "Xamarin";
-        public static string Password = "Password";
+        public static string Username = "";
+        public static string Password = "";
     }
 }

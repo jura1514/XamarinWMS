e076d4e94d0c99c8b22a8e4b79bc879128c386ca
George <George@DESKTOP-TEIVNGB>
2016-12-03 16:14:59 +0000

    dfdrdvdrvdrv

---
+++ WebWMS/Repository/DeliveryRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebWMS.Models;
+
+namespace WebWMS.Repository
+{
+    public class DeliveryRepository : IDeliveryRepository
+    {
+        static List<DeliveryModel> DeliveriesList = new List<DeliveryModel>();
+
+        public void Add(DeliveryModel del)
+        {
+            DeliveriesList.Add(del);
+        }
+
+        public DeliveryModel Find(int id)
+        {
+            return DeliveriesList
+                .Where(o => o.DeliveryId.Equals(id))
+                .SingleOrDefault();
+        }
+
+        public IEnumerable<DeliveryModel> GetAll()
+        {
+            return DeliveriesList;
+        }
+
+        public void Remove(int Id)
+        {
+            var del = DeliveriesList.SingleOrDefault(r => r.DeliveryId == Id);
+            if (del != null)
+                DeliveriesList.Remove(del);
+        }
+
+        public void Update(DeliveryModel del)
+        {
+            var delUpdate = DeliveriesList.SingleOrDefault(r => r.DeliveryId == del.DeliveryId);
+            if (delUpdate != null)
+            {
+                delUpdate.DeliveryId = del.DeliveryId;
+                delUpdate.Name = del.Name;
+                delUpdate.ExpectedDate = del.ExpectedDate;
+                delUpdate.Customer = del.Customer;
+                delUpdate.State = del.State;
+                delUpdate.StateChangeTime = del.StateChangeTime;
+            }
+        }
+    }
+}
---
+++ WebWMS/Repository/OrderRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebWMS.Models;
+
+namespace WebWMS.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        static List<OrderModel> OrderList = new List<OrderModel>();
+
+        public void Add(OrderModel order)
+        {
+            OrderList.Add(order);
+        }
+
+        public OrderModel Find(int id)
+        {
+            return OrderList
+                .Where(o => o.OrderId.Equals(id))
+                .SingleOrDefault();
+        }
+
+        public IEnumerable<OrderModel> GetAll()
+        {
+            return OrderList;
+        }
+
+        public void Remove(int Id)
+        {
+            var order = OrderList.SingleOrDefault(r => r.OrderId == Id);
+            if (order != null)
+                OrderList.Remove(order);
+        }
+
+        public void Update(OrderModel order)
+        {
+            var orderUpdate = OrderList.SingleOrDefault(r => r.OrderId == order.OrderId);
+            if (orderUpdate != null)
+            {
+                orderUpdate.OrderId = order.OrderId;
+                orderUpdate.OrderState = order.OrderState;
+                orderUpdate.Description = order.Description;
+                orderUpdate.StateChangeTime = order.StateChangeTime;
+            }
+        }
+    }
+}
